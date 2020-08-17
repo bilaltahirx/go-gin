fc3079af2f6a0c051996390401b4d82826009407
farehakaukab1 <fareha.kaukab@a10networks.com>
2020-08-17 07:19:03 +0500

    Nats push added

--- apis/users/handler.go
+++ apis/users/handler.go
@@ -1,22 +1,21 @@
 package users
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/bilaltahirx/go-gin/database"
 	"github.com/bilaltahirx/go-gin/model"
 	"github.com/bilaltahirx/go-gin/utils"
+	"github.com/gin-gonic/gin"
+	nats "github.com/nats-io/nats.go"
 	"golang.org/x/net/context"
 	"net/http"
 	"time"
-
-	//"context"
-	//"fmt"
-	//"github.com/bilaltahirx/go-gin/database"
-	//"github.com/bilaltahirx/go-gin/model"
-	"github.com/gin-gonic/gin"
-	//"time"
 )
 
 func RegisterUser(request *gin.Context) {
+	nc, _ := nats.Connect(nats.DefaultURL)
+
 	logger := utils.GetLogger()
 	_, cancel := context.WithTimeout(request, 2*time.Minute)
 	defer cancel()
@@ -38,6 +37,13 @@ func RegisterUser(request *gin.Context) {
 	}
 
 	logger.Info("Registering User")
+	user_json, err := json.Marshal(user)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	nc.Publish("foo", user_json)
 
 	request.JSON(200,user)
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-pg/pg/v9 v9.1.7
 	github.com/joho/godotenv v1.3.0
+	github.com/nats-io/nats.go v1.10.0
 	go.uber.org/zap v1.15.0
 	golang.org/x/net v0.0.0-20200222033325-078779b8f2d8
 )
